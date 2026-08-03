e7012f0d5158f0512ea2950d8514a5ebe1fd405b
haghdev <mhgorgab@outlook.com>
2026-08-03 09:05:53 +0330

    u

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -5,36 +5,32 @@ import '../storage/auth_storage.dart';
 class ApiClient {
   ApiClient._();
 
-  static final Dio dio = Dio(
-    BaseOptions(
-      baseUrl: "https://hajideligani.ir/api",
+  static final Dio dio =
+      Dio(
+          BaseOptions(
+            baseUrl: "https://hajideligani.ir/api",
 
-      connectTimeout: const Duration(seconds: 20),
+            connectTimeout: const Duration(seconds: 30),
+            sendTimeout: const Duration(minutes: 2),
+            receiveTimeout: const Duration(minutes: 2),
 
-      receiveTimeout: const Duration(seconds: 20),
+            headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json",
+            },
+          ),
+        )
+        ..interceptors.add(
+          InterceptorsWrapper(
+            onRequest: (options, handler) async {
+              final token = await AuthStorage.getToken();
 
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-    ),
-  )..interceptors.add(
-      InterceptorsWrapper(
-        onRequest: (
-          options,
-          handler,
-        ) async {
-          final token =
-              await AuthStorage.getToken();
+              if (token != null && token.isNotEmpty) {
+                options.headers["Authorization"] = "Bearer $token";
+              }
 
-          if (token != null &&
-              token.isNotEmpty) {
-            options.headers["Authorization"] =
-                "Bearer $token";
-          }
-
-          handler.next(options);
-        },
-      ),
-    );
+              handler.next(options);
+            },
+          ),
+        );
 }
